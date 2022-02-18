210b503c3c1913bff7745189056bef7dc7f89baa
xujinshan <xujinshan361@163.com>
2022-02-18 14:29:12 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/xujinshan/guliamll/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xujinshan.guliamll.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
