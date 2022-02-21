7569e5564b1a1df5949568b04f01e9e060260ff1
xujinshan <xujinshan361@163.com>
2022-02-21 18:50:59 +0800

    nacos 获取多配置&网关功能案例

---
+++ gulimall-gateway/src/main/java/com/xujinshan/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.xujinshan.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 开启服务注册发现
+ * 配置nacos的地址
+ */
+@EnableDiscoveryClient
+// 排除数据库配置，因为导入common会有mybatis
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
